4ebfaf047f72a4733e643cf25543522754829ddf
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-10-04 08:05:32 -0400

    unignored keys.js and cleared log.txt

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('keys is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Bhmpgr9EZrQuq1lf1HJkTdxE9',
+  consumer_secret: '4I90Ey8lWWjoF6gjdYRMmsTyGKSbBY9c8I7i2zGjGMVeCJPjos',
+  access_token_key: '913381179046416384-imSHhilbajWLrlhYnr2izqzeNZOOD8m',
+  access_token_secret: 'QPzvtAXETDFqYJeYpcWxXXsELwZMlRT8ncvePE4tF8mpa',
+}
+
+var spotifyKeys = {
+id: '043c65fe00a2462e8d569d5383b7ab4f',
+secret: '1aab282486af4d3dbb8c3b44ac6a9044',
+}
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+}
